837bd51ca70887faf636c52dff4fc13484bc33c9
René Chenard <rene.chenard1@ulaval.ca>
2023-03-08 19:35:13 -0500

    Implémentation d'une fenêtre glissante pour l'évaluation du score heuristique.

--- heuristic_evaluation.pl
+++ heuristic_evaluation.pl
@@ -33,58 +33,82 @@ heuristic_score(Board-Player-_, Score) :-
 	get_all_lines(Board, Lines),
 	clumped(Lines, RLE),
 	sum_score(Player, RLE, Score),
-	!,
 	hash_function(Board, Hash),
-	assertz(memo_heuristic_score(Hash, Goal, Score)).
+	assertz(memo_heuristic_score(Hash, Goal, Score)),
+	!.
 
 % Calcule la somme du score sur une ligne:
 sum_score(Player, Line, Score) :-
-	sum_score(Player, Line, 0, Score).
-sum_score(_, [], Score, Score) :- !.
-sum_score(Player, [X|Xs], PreviousScore, Score) :-
-	get_goal(Goal),
+	sum_score(Player, x, Line, 0, Score).
+sum_score(_, _, [], Score, Score) :- !.
+sum_score(_, _, [_], Score, Score) :- !.
+sum_score(_, _, [_, _], Score, Score) :- !.
+sum_score(Player, PS, [S1-N1, S2-N2, S3-N3|Rest], PreviousScore, Score) :-
 	(
-		( append(Pattern, _, [X|Xs]), pattern(Playing, Pattern, Type, N, Goal) ) ->
 		(
-			( Playing = Player -> Factor is 1 ; Factor is 10 ),
-			( Playing = n -> Sign = 1 ; Sign = -1 ),
-			value(Type, N, Goal, Value),
+			get_goal(Goal),
+			length(Rest, L),
+			(
+				L >= 1 ->
+				Rest = [NS-_|_]
+				;
+				NS = x
+			),
+			(
+				is_a_winning_row(Goal, PS, S1-N1, S2-N2, S3-N3, NS), value(win, N2, Value), Playing = S2
+				;
+				is_an_opened_row(Goal, PS, S1-N1, S2-N2, S3-N3, NS), value(opened, N2, Value), Playing = S2
+				;
+				is_a_closed_row(Goal, PS, S1-N1, S2-N2, S3-N3, NS), value(closed, N2, Value), Playing = S2
+				;
+				is_a_semi_opened_row_3(Goal, PS, S1-N1, S2-N2, S3-N3, NS), N is N1 + N3, value(semi_opened3, N, Value), Playing = S1
+				;
+				is_a_semi_opened_row_2(Goal, PS, S1-N1, S2-N2, S3-N3, NS), N is N1 + N3, value(semi_opened2, N, Value), Playing = S1
+				;
+				is_a_semi_opened_row_1(Goal, PS, S1-N1, S2-N2, S3-N3, NS), N is N1 + N3, value(semi_opened1, N, Value), Playing = S1
+			),
+			(
+				Playing = n, Sign = 1
+				;
+				Playing = b, Sign = -1
+			),
+			(
+				Playing = Player ->
+				Factor is 1
+				;
+				Factor is 10
+			),
 			NewScore is PreviousScore + Sign * Factor * Value
+			%writeln(NewScore)
 		)
 		;
 		(
 			NewScore is PreviousScore
 		)
 	),
-	sum_score(Player, Xs, NewScore, Score).
+	sum_score(Player, S1, [S2-N2, S3-N3|Rest], NewScore, Score).
 
-% Évite d'appliquer un patron aux délimiteurs (x) ou aux cases vides (v):
-is_a_player(Symbol) :- dif(Symbol, x), dif(Symbol, v), !.
+% Reconnaissance des patrons:
+is_a_winning_row(Goal, _, _, S2-N2, _, _) :- member(S2, [b, n]), Goal #=< N2, !.														% Rangée gagnante.
 
-% Pour capturer les rangées ouvertes:
-pattern(Playing, [v-A, Playing-N, v-B], opened, N, Goal) :- N #< Goal, Goal #=< A + N + B, dif(A, 1), dif(B, 1), is_a_player(Playing), !.
-pattern(Playing, [P-_, v-1, Playing-N, v-B], opened, N, Goal) :- N #< Goal, Goal #=< 1 + N + B, dif(P, Playing), is_a_player(Playing), !.
-pattern(Playing, [v-A, Playing-N, v-1, Q-_], opened, N, Goal) :- N #< Goal, Goal #=< A + N + 1, dif(Q, Playing), is_a_player(Playing), !.
-pattern(Playing, [P-_, v-1, Playing-N, v-1, Q-_], opened, N, Goal) :- N #< Goal, Goal #=< N + 2, dif(P, Playing), dif(Q, Playing), is_a_player(Playing), !.
+is_an_opened_row(Goal, _, v-N1, S2-N2, v-N3, _) :- member(S2, [b, n]), dif(N1, 1), dif(N3, 1), Goal #=< N1 + N2 + N3, !.				% Rangée ouverte avec deux espaces vides ou plus de chaque côtés.
+is_an_opened_row(Goal, PS, v-1, S2-N2, v-N3, _) :- member(S2, [b, n]), dif(PS, S2), dif(N3, 1), Goal #=< 1 + N2 + N3, !.				% Rangée ouverte avec un espace vide à gauche et deux espaces vides ou plus à droite.
+is_an_opened_row(Goal, _, v-N1, S2-N2, v-1, NS) :- member(S2, [b, n]), dif(N1, 1), dif(NS, S2), Goal #=< N1 + N2 + 1, !.				% Rangée ouverte avec un espace vide à droite et deux espaces vides ou plus à gauche.
+is_an_opened_row(Goal, PS, v-1, S2-N2, v-1, NS) :- member(S2, [b, n]), dif(PS, S2), dif(NS, S2), Goal #=< N2 + 2, !.					% Rangée ouverte avec un seul espace vide de chaque côtés.
 
-% Pour capturer les rangées fermées:
-pattern(Playing, [v-A, Playing-N, Y-_], closed, N, Goal) :- N #< Goal, other(Playing, Other), member(Y, [x, Other]), Goal #=< A + N, dif(A, 1), is_a_player(Playing), !.
-pattern(Playing, [X-_, Playing-N, v-B], closed, N, Goal) :- N #< Goal, other(Playing, Other), member(X, [x, Other]), Goal #=< N + B, dif(B, 1), is_a_player(Playing), !.
-pattern(Playing, [P-_, v-1, Playing-N, Y-_], closed, N, Goal) :- N #< Goal, other(Playing, Other), member(Y, [x, Other]), Goal #=< 1 + N, dif(P, Playing), is_a_player(Playing), !.
-pattern(Playing, [X-_, Playing-N, v-1, Q-_], closed, N, Goal) :- N #< Goal, other(Playing, Other), member(X, [x, Other]), Goal #=< N + 1, dif(Q, Playing), is_a_player(Playing), !.
+is_a_closed_row(Goal, _, v-N1, S2-N2, S3-_, _) :- member(S2, [b, n]), dif(N1, 1), dif(S3, v), Goal #=< N1 + N2, !.					% Rangée fermée à droite avec deux espaces vides ou plus à gauche.
+is_a_closed_row(Goal, _, S1-_, S2-N2, v-N3, _) :- member(S2, [b, n]), dif(S1, v), dif(N3, 1), Goal #=< N2 + N3, !.					% Rangée fermée à gauche avec deux espaces vides ou plus à droite.
+is_a_closed_row(Goal, PS, v-1, S2-N2, S3-_, _) :- member(S2, [b, n]), dif(PS, S2), dif(S3, v), Goal #=< 1 + N2, !.					% Rangée fermée à droite avec un seul espace vide à gauche.
+is_a_closed_row(Goal, _, S1-_, S2-N2, v-1, NS) :- member(S2, [b, n]), dif(S1, v), dif(NS, S2), Goal #=< 1 + N2, !.					% Rangée fermée à gauche avec un seul espace vide à droite.
 
-% Pour capturer les rangées semi-ouvertes:
-pattern(Playing, [v-_, Playing-M, v-1, Playing-N, v-_], semi_opened3, S, Goal) :- S #= M + N, Goal #=< S + 1, is_a_player(Playing), !.
-pattern(Playing, [P-_, Playing-M, v-1, Playing-N, Q-_], semi_opened2, S, Goal) :- S #= M + N, Goal #=< S + 1, ( dif(P, v) ; dif(Q, v) ), is_a_player(Playing), !.
-pattern(Playing, [P-_, Playing-M, v-1, Playing-N, Q-_], semi_opened1, S, Goal) :- S #= M + N, Goal #=< S + 1, ( dif(P, v) , dif(Q, v) ), is_a_player(Playing), !.
-
-% Pour capturer une rangée gagnante:
-pattern(Playing, [Playing-N], win, _, Goal) :- Goal #=< N, is_a_player(Playing), !.
+is_a_semi_opened_row_3(Goal, v, P-N1, v-1, P-N3, v) :- member(P, [b, n]), Goal #=< N1 + 1 + N3, !.									% Rangée semi-ouverte des deux côtés.
+is_a_semi_opened_row_2(Goal, PS, P-N1, v-1, P-N3, NS) :- member(P, [b, n]), ( dif(PS, v) ; dif(NS, v) ), Goal #=< N1 + 1 + N3, !.	% Rangée semi-ouverte d'un seul côté.
+is_a_semi_opened_row_1(Goal, PS, P-N1, v-1, P-N3, NS) :- member(P, [b, n]), dif(PS, v), dif(NS, v), Goal #=< N1 + 1 + N3, !.			% Rangée semi-ouverte au centre seulement.
 
 % Pour évaluer la valeur d'un patron:
-value(win, _, Goal, Value) :- Value is 3 * 10**(Goal - 1), !.
-value(opened, N, _, Value) :- Value is 3 * 10**(N - 1), !.
-value(closed, N, _, Value) :- Value is 1 * 10**(N - 1), !.
-value(semi_opened3, N, _, Value) :- Value is 2 * 10**(N - 1), !.
-value(semi_opened2, N, _, Value) :- Value is 8 * 10**(N - 2), !.
-value(semi_opened1, N, _, Value) :- Value is 5 * 10**(N - 2), !.
+value(win, N, Value) :- Value is 3 * 10**(N - 1), !.
+value(opened, N, Value) :- Value is 3 * 10**(N - 1), !.
+value(closed, N, Value) :- Value is 1 * 10**(N - 1), !.
+value(semi_opened3, N, Value) :- Value is 2 * 10**(N - 1), !.
+value(semi_opened2, N, Value) :- Value is 8 * 10**(N - 2), !.
+value(semi_opened1, N, Value) :- Value is 5 * 10**(N - 2), !.
